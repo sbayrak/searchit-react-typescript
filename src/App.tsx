cd443c5db1b42c0063c979679476a6d2df1b43b1
sbayrak <34121748+sbayrak@users.noreply.github.com>
2022-03-16 22:51:42 +0200

    added scroll top component on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './components/pages/Home';
 import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Contact from './components/pages/Contact';
 import Search from './components/pages/Search';
 import NotFound from './components/pages/NotFound';
+import ScrollTop from './components/layout/ScrollTop';
 
 function App() {
   return (
     <Router>
       <div className='container-lg' style={{ border: '1px solid red' }}>
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
-          <Route path='/search' element={<Search />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
+        <ScrollTop>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+            <Route path='/search' element={<Search />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </ScrollTop>
       </div>
       <Footer />
     </Router>
